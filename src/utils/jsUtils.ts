9a14fefc3cfab798e17907aec5961a8258595ca6
Psilo <psilo@unterrainer.info>
2021-09-20 13:50:54 +0200

    several refactorings

--- src/utils/jsUtils.ts
+++ src/utils/jsUtils.ts
@@ -1,30 +1,32 @@
-export function toggleItem (item, array) {
-  const index = array.indexOf(item)
-  if (index === -1) {
-    array.push(item)
-  } else {
-    array.splice(index, 1)
-  }
-}
+export default {
+  toggleItem: function (item, array) {
+    const index = array.indexOf(item)
+    if (index === -1) {
+      array.push(item)
+    } else {
+      array.splice(index, 1)
+    }
+  },
 
-export function removeItem (item, array) {
-  const index = array.indexOf(item)
-  if (index !== -1) {
-    array.splice(index, 1)
-  }
-}
+  removeItem: function (item, array) {
+    const index = array.indexOf(item)
+    if (index !== -1) {
+      array.splice(index, 1)
+    }
+  },
 
-export function containsItem (item, array) {
-  return array.indexOf(item) !== -1
-}
+  containsItem: function (item, array) {
+    return array.indexOf(item) !== -1
+  },
 
-/**
- * Returns the value itself, or an empty string, if the value was null or undefined.
- * @param value the value to sanitize
- */
-export function sanitize (value) {
-  if (value === null || value === undefined) {
-    return ''
+  /**
+   * Returns the value itself, or an empty string, if the value was null or undefined.
+   * @param value the value to sanitize
+   */
+  sanitize: function (value) {
+    if (value === null || value === undefined) {
+      return ''
+    }
+    return value
   }
-  return value
 }
