350b9f2b54a5cbf299f9058c3527a74dfc76ff9e
Gerald <gerald@unterrainer.info>
2023-02-03 11:20:35 +0100

    add mastodon verification link

--- src/components/footerElement.vue
+++ src/components/footerElement.vue
@@ -1,10 +1,19 @@
 <template>
   <v-footer padless color="transparent">
     <v-card flat tile width="100%" height="50px" class="text-center mt-8">
-      <v-card-text
-        class="grey--text"
-        v-html="`${$t('resource.footerCopyright')} ${new Date().getFullYear()}`"
-      ></v-card-text>
+      <v-row align="center">
+        <v-col>
+          <v-card-text
+            class="grey--text"
+            v-html="`${$t('resource.footerCopyright')} ${new Date().getFullYear()}`"
+          ></v-card-text>
+        </v-col>
+        <v-col>
+          <a rel="me" href="https://det.social/@psilo" target="_blank">
+            <v-btn x-small plain>Mastodon</v-btn>
+          </a>
+        </v-col>
+      </v-row>
     </v-card>
   </v-footer>
 </template>
